86a07b3fd015f923206a1c0102ee4ac5b18fcba2
kimu-ari <nijiriyorukage@yahoo.co.jp>
2020-11-20 16:12:46 +0900

    providerパターンでモデルを作ってみる

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/main_model.dart';
 import 'package:flutter_app/next_page.dart';
+import 'package:provider/provider.dart';
 
-
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: ChangeNotifierProvider<MainModel> (
+        create: (_) => MainModel(),
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('コリアンダー'),
+          ),
+          body: Consumer<MainModel> (builder: (context, model, child) {
+              return Center(
+                child: Column(
+                  children: [
+                    Text(
+                        model.kboyText,
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                    RaisedButton(
+                      child: Text('ボタン'),
+                      onPressed: () {
+                        //ここで何か
+                        model.changeKboyText();
+                      },
+                    )
+                  ],
+                ),
+              );
+            }
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter'),
-
     );
   }
+
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final items = List<String>.generate(10000, (i) => "Item $i");
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('KBOYのFlutter大学'),
-      ),
-      body: Container(
-        width: double.infinity,
-        child: ListView(
-          // This next line does the trick.
-          scrollDirection: Axis.horizontal,
-          children: <Widget>[
-            Container(
-              width: 160.0,
-              color: Colors.red,
-            ),
-            Container(
-              width: 160.0,
-              color: Colors.blue,
-            ),
-            Container(
-              width: 160.0,
-              color: Colors.green,
-            ),
-            Container(
-              width: 160.0,
-              color: Colors.yellow,
-            ),
-            Container(
-              width: 160.0,
-              color: Colors.orange,
-            ),
-          ],
-        )
-
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
